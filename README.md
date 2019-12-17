6c2da6c3aba8ba4a025a4f9b8951ecf1a543ae87
roberts <adam.roberts@targomo.com>
2019-12-17 22:30:53 +0100

    readme

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # micro-indeterminate-progress-bar
 miniature library for adding an indeterminate progress bar
 
+Only need a simple progress bar to show that something is loading/downloading? 
+
+## Installation
+Include in script tag:
+
+```
+<script src="dist/mipb.min.js"></script>
+```
+
+this exposes the global `mipb` class
+
+## Use
+```
+var pBar = new mipb(options)
+```
+
+this instantiates the progress bar, and appends a micro-snippet of css to the document `<head>`
+
+### Options
+```
+defaults = {
+    bg: "transparent", //background color for the bar
+    fg: "#f00", // foreground color for the bar
+    frame: 2, // how many seconds the animation should take
+    selector: "body", // where to append the bar
+    height: 3 // bar height in px
+};
+```
+
+### show bar
+```
+pBar.show()
+```
+
+### hide bar
+```
+pBar.hide()
+```
+
+### toggle bar
+```
+pBar.toggle()
+```
--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ class mipb {
     constructor(_options) {
         this.visible = false;
         const defaults = {
-            bg: "transparent",
-            fg: "#f00",
-            frame: 2,
-            selector: "body",
-            height: 3
+            bg: "transparent", //background color for the bar
+            fg: "#f00", // foreground color for the bar
+            frame: 2, // how many seconds the animation should take
+            selector: "body", // where to append the bar
+            height: 3 // bar height in px
         };
         const options = {
             ...defaults,
             ..._options
         };
+        // has the stylesheet been appended already?
         const existing = document.getElementById("mipb-stylesheet");
         if (existing == null) {
+            // create and append stylesheet
             const linkElement = document.createElement("link");
             linkElement.setAttribute("id", "mipb-stylesheet");
             linkElement.setAttribute("rel", "stylesheet");
@@ -31,32 +33,36 @@ class mipb {
             );
             document.head.appendChild(linkElement);
         }
-        
+        // create the progress container
         this.element = document.createElement("div");
         this.element.style.backgroundColor = options.bg;
+
+        // create the animated progress bar
         const throbber = document.createElement("div");
-        throbber.style.position = "absolute";
-        throbber.style.top = 0;
-        throbber.style.right = 0;
-        throbber.style.left = 0;
-        throbber.style.height = `${options.height}px`;
-        throbber.style.transform = "translateX(-100%)";
         throbber.style.backgroundColor = options.fg;
+
+        // add bar to container
         this.element.appendChild(throbber);
         this.element.classList.add("mipb-outer");
+
+        // add the entire thing to the target caontainer
         document.querySelector(options.selector).appendChild(this.element);
     }
+    // is the bar currently visible ?
     isVisible() {
         return this.visible;
     }
+    // hide the bar
     hide() {
         this.visible = false;
         this.element.classList.remove("show");
     }
+    // show the bar
     show() {
         this.visible = true;
         this.element.classList.add("show");
     }
+    // toggle bar visible/non-visible
     toggle() {
         if (this.visible) {
             this.hide();
